e9d346aeca5ed6a5b9ff1b89b791c924c9c92c3e
Sahil <physics11221@gmail.com>
2022-05-24 15:18:02 +0530

    https://github.com/AdvanceGamer/React-Project1

--- src/Project2/Project2.js
+++ src/Project2/Project2.js
@@ -13,7 +13,7 @@ export default function Project2(){
 <Card img={require('./image 12.png')} rating="5" review="6" about="Life lesson with Katie Jaferes" cost="$136" status="Sold Out"/>
 <Card img={require('./wedding-photography 1.png')} rating="5" review="30" about="Learn wedding photography" cost="$125" status="Online"/>
 <Card img={require('./mountain-bike 1.png')} rating="4.8" review="2" about="Group Mountain Biking" cost="$50" status="Online"/>
-<Card img={require('./IMG_20220401_145616~2.jpg')} rating="5" review="10K" about="Learn Chess with Sahil the gold medalist 🥇" cost="$100" status="Available"/>
+<Card img={require('./IMG_20220401_145616~2.jpg')} rating="5" review="10M" about="Learn Chess with Sahil the gold medalist 🥇" cost="$100" status="Available"/>
 </div>
         </div>
     )
